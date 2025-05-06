31a95a2426916954c49c407f08ec42145bf1132e
meenarao01 <meenaregulagedda@gmail.com>
2025-05-06 21:09:42 +1000

    Added Hello world print and bm11 method

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -3,7 +3,13 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello world");
 
+	}
+	public void bm11() {
+		System.out.println("This is my first branch that is printing");
+		System.out.println("This is my seond branch");// second piece of code
+		
 	}
 
 }
